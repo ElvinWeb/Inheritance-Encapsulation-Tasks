23648b7701754a71efc2cf3fa41da14bc4dcbf0e
Elvin <elvinsrkrov@gmail.com>
2023-10-10 10:26:04 +0400

    added the Encapsulation and Inhertiance tasks

---
+++ EncapsulationAndInheritanceTasks/EncapsulationAndInheritanceTasks/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationAndInheritanceTasks
+{
+    public class Person
+    {
+        private string _fullName;
+        private int _age;
+        public double PhoneNumber;
+
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                string fullNameVal = value;
+                fullNameVal = fullNameVal.Trim();
+
+                for (int i = 0; i < fullNameVal.Length; i++)
+                {
+                    if (fullNameVal[i] == ' ')
+                    {
+                        if (char.IsUpper(fullNameVal[i + 1]) && char.IsUpper(fullNameVal[0]))
+                        {
+                            this._fullName = fullNameVal;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    this._age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age deyeri menfi ola bilmez");
+                }
+
+            }
+        }
+        
+
+        public Person(string fullName, int age, double phoneNumber)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+            this.PhoneNumber = phoneNumber;
+        }
+        public void showAllData()
+        {
+            Console.WriteLine($"FullName: {FullName} Age: {Age} PhoneNumber: {PhoneNumber}");
+        }
+    }
+}
---
+++ EncapsulationAndInheritanceTasks/EncapsulationAndInheritanceTasks/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EncapsulationAndInheritanceTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person(" Elvin Sarkarov  ", 20, 0553453434);
+            //person.showAllData();
+
+
+
+            string strVal = Console.ReadLine();
+            Console.WriteLine(Capitalize(strVal));
+
+        }
+
+        static string Capitalize(string word)
+        {
+            string capitalWord = "";
+            word = word.Trim();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsUpper(word[0]))
+                {
+                    capitalWord = word;
+                    break;
+                }
+                else
+                {
+                    capitalWord = char.ToUpper(word[0]) + word.Substring(1);
+                }
+            }
+            return capitalWord;
+        }
+    }
+}
